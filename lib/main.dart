b00f04b1a060d42d89d35d0263ba10c157f33d0d
Yash Choudhari <yashchoudhari.ysc@gmail.com>
2021-02-23 23:57:09 +0530

    Provide structuring to the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite colour?',
+      'answers': [
+        {'text': 'Red', 'score': 9},
+        {'text': 'Green', 'score': 5},
+        {'text': 'Yellow', 'score': 4},
+        {'text': 'Pink', 'score': 2},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Cat', 'score': 2},
+        {'text': 'Lion', 'score': 10},
+        {'text': 'Dog', 'score': 4},
+        {'text': 'Tiger', 'score': 8},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favourite Game?',
+      'answers': [
+        {'text': 'GTA', 'score': 1},
+        {'text': 'PUBG', 'score': 5},
+        {'text': 'CyberPunk', 'score': 8},
+        {'text': 'ApexLegend', 'score': 9},
+      ],
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -25,21 +57,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    final _questions = const [
-      {
-        'questionText': 'What\'s your favourite colour?',
-        'answers': ['Red', 'Green', 'Yellow', 'Pink'],
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Cat', 'Lion', 'Dog', 'Tiger'],
-      },
-      {
-        'questionText': 'What\'s your favourite Game?',
-        'answers': ['GTA', 'PUBG', 'CyberPunk', 'ApexLegend'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -51,7 +68,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome and innocent!';
+    } else if (resultScore <= 15) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 20) {
+      resultText = 'you are ... strange!';
+    } else {
+      resultText = 'You are so bad!';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
