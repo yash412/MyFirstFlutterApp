543afc23a1f12bf08923e556fd0ab5a269b1ae0d
Yash Choudhari <yashchoudhari.ysc@gmail.com>
2021-02-23 21:35:14 +0530

    fixed crashing problem in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -43,21 +43,24 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My first App'),
-          ),
-          body: Column(
-            children: [
-              Question(
-                questions[_questionIndex]['questionText'],
-              ),
-              // ... means spread operator
-              ...(questions[_questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                return Answer(_answerQuestion, answer);
-              }).toList()
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  // ... means spread operator
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(child: Text('You did it!')),
+      ),
     );
   }
 }
